b02bd1352237b2e598d71e581618b6a7cae216d9
itoakira04 <k021c1523@g.neec.ac.jp>
2024-09-10 15:08:38 +0900

    2

--- mainmenu.py
+++ mainmenu.py
@@ -3,21 +3,27 @@ import tkinter as tk
 
 #tkinterオブジェクトの取得
 window = tk.Tk()
+
 #ウィンドウのタイトル設定
 window.title("新人研修音声解析システム")
+
 #ウィンドウの表示サイズ設定
 window.geometry("1000x600")
+
 #ラベル
 label1 = tk.Label(text = '以下のボタンをクリックしてください',font = ("Helvetica","20","bold") )
-label1.pack()
+label1.place(x=250,y=35)
+
 #録音画面ボタン
-Button_REC = tk.Button(text = "録音",width=50)
-Button_REC.pack()
+Button_REC = tk.Button(text = "録音",width=20, height=2, font = ("Helvetica","20","bold"), bg = "#d4d4d4")
+Button_REC.place(x = 325,y = 100)
+
 #分析結果表示ボタン
-Button_result = tk.Button(text = "分析結果表示",width=50)
-Button_result.pack()
+Button_result = tk.Button(text = "分析結果表示",width=20, height=2, font = ("Helvetica","20","bold"), bg = "#d4d4d4")
+Button_result.place(x = 325 , y = 250)
+
 #研修生編集ボタン
-Button_memuber = tk.Button(text = "研修生編集",width=50)
-Button_memuber.pack()
+Button_memuber = tk.Button(text = "研修生編集",width=20, height=2, font = ("Helvetica","20","bold"), bg = "#d4d4d4")
+Button_memuber.place(x = 325 , y = 400)
 
 window.mainloop()
