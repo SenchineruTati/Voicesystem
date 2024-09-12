109205f824a8b69f70d4f91bb4dad6a187b1eb8b
itoakira04 <k021c1523@g.neec.ac.jp>
2024-09-12 15:51:12 +0900

    commit

---
+++ MemberList.py
@@ -0,0 +1,17 @@
+import sys
+import tkinter as tk
+import matplotlib.pyplot as plt
+import pandas as pd
+
+#tkinterオブジェクトの取得
+window = tk.Tk()
+
+#ウィンドウのタイトル設定
+window.title("新人研修音声解析システム")
+
+#ウィンドウの表示サイズ設定
+window.geometry("1000x600")
+
+#表の作成
+
+window.mainloop()
